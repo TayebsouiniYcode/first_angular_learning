a402b9ae2680b982f835262e3a6b72ca99a5560d
TayebsouiniYcode <TayebsouiniYcode@gmail.com>
2022-12-26 16:50:03 +0100

    initial commit angular learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarketComponent } from './market/market.component';
+import { CollectionComponent } from './collection/collection.component';
+import {CollectableService} from "./shared/collectable.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketComponent,
+    CollectionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CollectableService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
